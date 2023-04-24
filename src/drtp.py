a9db26373f1c8d97cd03fa26a191969f4ea40d8a
PetterHalsne <s320947@oslomet.no>
2023-04-24 14:52:08 +0200

    kommentarer for hovedfil

---
+++ src/drtp.py
@@ -0,0 +1,11 @@
+#I denne filen skal vi ha nødvendig kode for server og client + DRTP (men ikke header, den er i en egen fil)
+#Flags:
+#-s -> server
+#-c -> client
+#-I -> client ip
+#-b -> server ip
+#-p -> port
+#-f -> filename?
+#-m -> mode (hvilken metode i DRTP for server)
+#-r -> hvilken metode client skal sende på i DRTP
+#-t -> Forskjell mellom client og server: Skipack er servermetode, loss er clientmetode ?
