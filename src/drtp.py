4f861ade2ddb4ff8c046fd5fa31aab0ccc7b55ae
Miski Yusuf <s364593@oslomet.no>
2023-04-25 13:55:51 +0200

    endring

--- src/drtp.py
+++ src/drtp.py
@@ -1,10 +1,5 @@
 #I denne filen skal vi ha nødvendig kode for server og client + DRTP (men ikke header, den er i en egen fil)
 #Flags:
-#-s -> server
-#-c -> client
-#-I -> client ip
-#-b -> server ip
-#-p -> port
 #-f -> filename?
 #-m -> mode (hvilken metode i DRTP for server)
 #-r -> hvilken metode client skal sende på i DRTP
@@ -30,11 +25,47 @@ import socket
 import sys
 import re #Importing regex to check ip-adress for errors
 
+socket.settimeout(500) #The default timeout for any socket operation is 500 ms.
+
+def handshakeServer():
+    modifiedMessage, (serverip, port) = clientSocket.recvfrom(2048)
+    modifiedMessage = modifiedMessage.decode()
+    
+
+
+def handshakeClient(clientSocket, serverip, port, method, fileForTransfer): #Sends an empty package with a header containing the syn flag. Waits for a ack from the server with a timeout of 500 ms.
+    
+    sequence_number = 0
+    acknowledgment_number = 0
+    window = 64000
+    flags = 8
+    data = 0
+
+    msg = header.create_packet(sequence_number, acknowledgment_number, flags, window, data)
+    clientSocket.sendto(msg, (serverip, port))
+    
+    modifiedMessage, (serverip, port) = clientSocket.recvfrom(2048)
+    modifiedMessage = modifiedMessage.decode()
+    
+    data_from_msg = modifiedMessage[12:]
+    seq, acknum, flags, win = header.parse_header (data_from_msg) #it's an ack message with only the header
+    syn, ack, fin = header.parse_flags(flags)
+
+    if syn and ack:
+        flags= 4
+       
+        msg = header.create_packet(sequence_number,acknowledgment_number,flags,window,data)
+        clientSocket.sendto(msg, (serverip, port))
+        transmittAndListen(clientSocket, fileForTransfer, method)
+
+def transmittAndListen(msg):
+    print("Her skal vi sende og lytte alt etter metode som ble satt i terminalen")
+
 def check_IP(ip_address): #Code to check that the ip adress is valid. Taken from https://www.abstractapi.com/guides/python-regex-ip-address. Comments added by us.
 
     if not re.search(r"[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}", ip_address): #Check that the format is like this: "XXX.XXX.XXX.XXX", where X is a number between 0 and 9.
         raise Exception(f"The IP address {ip_address} is not valid. It needs to be in the format X.X.X.X, where each X is a number from 0 to 255.")
-   
+
     ip_split = ip_address.split(".") #Splits the IP-adress string based on the periods, and in the for loop checks that each byte is from 0 to 255.
   
     for ip_part in ip_split:
@@ -53,6 +84,8 @@ def check_port(port): #Code to check that the port is written is valid. Inspired
 
 def createServer(ip, port):
     print("Her opprettes server:")
+    handshakeServer()
+
     serverSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     serverSocket.bind((ip, port))
     print('The server is ready to receive')
@@ -61,53 +94,26 @@ def createServer(ip, port):
         message, clientAddress = serverSocket.recvfrom(2048)
         message = message.decode()
         data_from_msg = message[12:]
-        header.parse_header(data_from_msg)
-
-        #Vil sende melding tilbake:
-        data = b'' 
-        print('\n\nCreating an acknowledgment packet:')
-        print (f'this is an empty packet with no data ={len(data)}')
+        Recieved_header = header.parse_header(data_from_msg)
+        print(Recieved_header)
+        data = b''
 
         sequence_number = 0
-        acknowledgment_number = 1
+        acknowledgment_number = 0
         window = 0 
         flags = 4
 
         msg = header.create_packet(sequence_number, acknowledgment_number, flags, window, data)
-        print (f'this is an acknowledgment packet of header size={len(msg)}')
         serverSocket.sendto(msg.encode(), clientAddress)
 
-        seq, ack, flags, win = header.parse_header (msg) #it's an ack message with only the header
-        print(f'seq={seq}, ack={ack}, flags={flags}, receiver-window={win}')
-    
-      
-        
-        
+        seq, acknum, flags, win = header.parse_header (msg) #it's an ack message with only the header
+        print(f'seq={seq}, ack={acknum}, flags={flags}, receiver-window={win}') #TODO delete this
 
-def createClient(serverip, port):
+def createClient(serverip, port, method, fileForTransfer):
     print("Her opprettes client:")
-
     clientSocket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    handshakeClient(clientSocket, serverip, port, method, fileForTransfer) #Sending a packet with the syn flag to the server, if an ack is recieved transmission of data starts.
     
-    #Message to server from client: 
-    data = b'0' * 1460
-    print (f'app data for size ={len(data)}')
-    sequence_number = 1
-    acknowledgment_number = 0
-    window = 0 
-    flags = 0
-    msg = header.create_packet(sequence_number, acknowledgment_number, flags, window, data)
-
-    #Encoding packet and sending it to server ip and port
-    clientSocket.sendto(msg, (serverip, port))
-    
-    modifiedMessage, (serverip, port) = clientSocket.recvfrom(2048)
-    modifiedMessage = modifiedMessage.decode()
-    data_from_msg = modifiedMessage[12:]
-    header.parse_header(data_from_msg)
-    seq, ack, flags, win = header.parse_header (data_from_msg) #it's an ack message with only the header
-    print(f'seq={seq}, ack={ack}, flags={flags}, receiver-window={win}')
-    clientSocket.close()
 
 #Defining the argumentParser
 parser = argparse.ArgumentParser(description='The arguments used when calling the program')
@@ -125,6 +131,8 @@ args = parser.parse_args()
 serverip = args.serverip
 bind = args.bind
 port = args.port
+method = "Metode som hentes ut av argparse"
+fileForTransfer = "Fil som skal sendes"
 
 if args.client == True or args.server == True:
     if(args.client == True and args.server == True):
@@ -133,12 +141,11 @@ if args.client == True or args.server == True:
     else:
         if args.client == True:
             if(check_port(port) and check_IP(serverip)):
-                createClient(serverip, port)
+                createClient(serverip, port, method, fileForTransfer)
         if(args.server == True):
             if(check_port(port) and check_IP(bind)):
                 createServer(bind, port)
 else:
     print("You have to use either the -s (server) og -c (client) flag.")
     sys.exit()
-
-#Udp client
+#UDP client
