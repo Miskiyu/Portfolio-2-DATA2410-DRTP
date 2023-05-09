f5b044e1af3a89c8139361811fd3c206143aa661
N4h0 <89570972+N4h0@users.noreply.github.com>
2023-05-10 00:13:45 +0200

    filalfinal edit???

--- src/compare.py
+++ src/compare.py
@@ -1,17 +1,17 @@
-#A simple program to compare two files, made entierly by chatGPT
+#A simple program to compare two files. 
 
 import sys
 
-def read_file(file_path):
-    with open(file_path, 'r', encoding='utf-8', errors='replace') as file:
+def read_file(file):
+    with open(file, 'r', encoding='utf-8', errors='replace') as file:
         content = file.read()
     return content
 
 def compare_files(file1, file2):
-    content1 = read_file(file1)
-    content2 = read_file(file2)
+    fil1 = read_file(file1)
+    fil2 = read_file(file2)
     
-    if content1 == content2:
+    if fil1 == fil2:
         print("The files are identical.")
     else:
         print("The files are different.")
